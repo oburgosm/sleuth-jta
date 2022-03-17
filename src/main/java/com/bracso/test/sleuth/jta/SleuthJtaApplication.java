8817703488e46024fdb3c7f0e1abed790c828c36
oburgosm <oscarbma@ext.inditex.com>
2022-03-17 09:24:14 +0100

    Changes

---
+++ src/main/java/com/bracso/test/sleuth/jta/SleuthJtaApplication.java
@@ -0,0 +1,13 @@
+package com.bracso.test.sleuth.jta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SleuthJtaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SleuthJtaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bracso/test/sleuth/jta/SleuthJtaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bracso.test.sleuth.jta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SleuthJtaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
